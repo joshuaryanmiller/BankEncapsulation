bca779b9e5e33e53cd9dd1bd323202c479ec50ee
Joshua Miller <joshua.ryan.miller@icloud.com>
2022-10-14 11:37:30 -0700

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double _balance = 0;
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+            Console.WriteLine($"You have successfully deposited {amount, 0:c}.");
+        }
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
